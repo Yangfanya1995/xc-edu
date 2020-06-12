4e8c06d80e7d382fea34b1d1ce225f38d0c1207c
21up <1109287523@qq.com>
2020-06-12 13:38:56 +0800

    新增cmsmanage项目

---
+++ back_end/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/dao/CmsPageRepository.java
@@ -0,0 +1,12 @@
+package com.xuecheng.manage_cms.dao;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CmsPageRepository extends MongoRepository<CmsPage,String>{
+    //分页查询
+    @Override
+    Page<CmsPage> findAll(Pageable pageable);
+}
